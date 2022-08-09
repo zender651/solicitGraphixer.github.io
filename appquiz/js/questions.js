d85b6256ebc729cd91fac425cb5ffbd7743f2529
zender651 <nbpvm12engg@gmail.com>
2022-08-09 15:18:48 +0530

    my website(quiz app)
    my portfolio website

---
+++ appquiz/js/questions.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Which is the Largest Delta in India?",
+    answer: "Sunderbans",
+    options: [
+      "Sunderbans",
+      "Podducherry",
+      "Gulf of Khachh",
+      "Lakshwadeep"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which is called, the City of Golden Temple?",
+    answer: "Amritsar",
+    options: [
+      "Delhi",
+      "Amritsar",
+      "Kolkata",
+      "Patna"
+    ]
+  },
+    {
+    numb: 3,
+    question: " Which is the Largest River?",
+    answer: "Nile",
+    options: [
+      "Amazon",
+      "Ganga",
+      "Nile",
+      "yantze"
+    ]
+  },
+    {
+    numb: 4,
+    question: " Which is the Highest Plateau?",
+    answer: "Pamir(Tibet)",
+    options: [
+      "Pamir(Tibet)",
+      "China",
+      "Grand Canyon",
+      "Deccan plateau"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Which is called, God's Own Country?",
+    answer: "Kerala",
+    options: [
+      "Maldives",
+      "Andaman and Nicobar",
+      "Lakshwadeep",
+      "Norway"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
